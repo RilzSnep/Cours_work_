83390ee37199e2679abd84b0ef7856a6543585bf
Zifirka <Ilaafanasef21@gmail.com>
2024-06-29 17:05:33 +0300

    2.3 поиск готов

--- src/reports.py
+++ src/reports.py
@@ -1,6 +1,5 @@
 import json
 import logging
-import re
 from typing import Any, Dict, List
 
 import pandas as pd
@@ -24,43 +23,47 @@ def read_transactions_xlsx(file_path: str) -> Any:
 
 
 # Функция для простого поиска
-def simple_search(transactions: Any, search_string: str) -> List[Dict]:
-    """!!ARGS of functions:
-    transactions: pd.DataFrame,
-                         category: str,
-                         date: Optional[str] = None) -> pd.DataFrame:
+def simple_search(transactions: List[Dict], search_data: List[Dict]) -> List[Dict]:
     """
-    """
-    Простой поиск транзакций по описанию.
+    Простой поиск транзакций по данным из JSON.
 
     Args:
         transactions: Список словарей с транзакциями.
-        search_string: Строка поиска.
+        search_data: Список словарей с данными для поиска.
 
     Returns:
         Список словарей с транзакциями, соответствующими запросу.
     """
-    logger.info(f"Поиск транзакций по строке '{search_string}'")
-    return [
-        transaction
-        for transaction in transactions
-        if "description" in transaction and re.search(search_string, transaction["description"])
-    ]
+    logger.info(f"Поиск транзакций по данным: {search_data}")
+
+    filtered_transactions = []
+    for search_item in search_data:
+        for transaction in transactions:
+            if all(item in transaction.items() for item in search_item.items()):
+                filtered_transactions.append(transaction)
+    return filtered_transactions
 
 
 def main_reports() -> None:
     """
-    Главная функция модуля, объединяющая все функции в 1
-
-    :return: None
+    Главная функция модуля.
     """
     operations = read_transactions_xlsx("../data/operations_mi.xls")
-    print("Ведите что нужно найти")
+    print("Введите данные для поиска в формате JSON:")
+    # [{"name": "Алексей"}]
     search_string = input()
-    filtered_operations = simple_search(operations, search_string)
+
+    try:
+        search_data = json.loads(search_string)
+    except json.JSONDecodeError:
+        logger.error("Некорректный формат JSON.")
+        print("Ошибка: некорректный формат JSON.")
+        return
+
+    filtered_operations = simple_search(operations, search_data)
 
     with open("filtered_operations.json", "w", encoding="utf-8") as f:
-        json.dump(filtered_operations, f, indent=4, ensure_ascii=False)  # indent для красивого формата
+        json.dump(filtered_operations, f, indent=4, ensure_ascii=False)
 
     logger.info("Отфильтрованные операции записаны в файл filtered_operations.json")
     print("Отфильтрованные операции записаны в файл filtered_operations.json")
